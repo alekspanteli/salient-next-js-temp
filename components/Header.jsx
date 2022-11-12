b5e1ca74b9b47596c6a1941c73d8f7b920cae040
alekspanteli <alekspanteli@rambler.ru>
2022-11-12 14:57:21 +0000

    add js config

---
+++ components/Header.jsx
@@ -0,0 +1,13 @@
+import { Container } from '@/components/Container'
+
+export default function Header() {
+    return (
+        <>
+            <header className="py-10">
+                <Container>
+                    Header goes here
+                </Container>
+            </header>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-
+import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <div>test</div>
+    <>
+    <Head>
+        <title>Home page</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+
+      <Header />
+    </>
   )
 }
