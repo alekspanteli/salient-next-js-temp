d5da0604b74dd554d8433db6d9fbcebde35c7ec1
alekspanteli <alekspanteli@rambler.ru>
2022-11-14 12:42:51 +0000

    update font

--- pages/_document.js
+++ pages/_document.js
@@ -8,15 +8,8 @@ export default function Document() {
         >
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="anonymous"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Lexend:wght@400;500&display=swap"
-                />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;500;600;700;800;900&display=swap" rel="stylesheet" />
             </Head>
             <body className="flex h-full flex-col">
                 <Main />
