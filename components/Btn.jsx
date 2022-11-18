cc72755f9d19d899c99f6887e29aa8f6572f98c0
alekspanteli <alekspanteli@rambler.ru>
2022-11-18 14:44:47 +0000

    test

--- components/Btn.jsx
+++ components/Btn.jsx
@@ -2,14 +2,27 @@ import Link from 'next/link'
 import clsx from 'clsx'
 
 const baseStyles = {
-    solid: 'inline-flex items-center justify-center py-2 px-4 text-sm',
-    outline:'inline-flex items-center justify-center py-2 px-4 text-sm bg-red',
+    solid:
+        'group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2',
+    outline:
+        'group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-none',
 }
 
-export default function Button({ variant = 'solid', className, href, ...props }) {
+const variantStyles = {
+    solid: {
+        blue: 'bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900',
+    },
+    outline: {
+        slate:
+            'ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300',
+    },
+}
+
+export default function Button({ variant = 'solid', color = 'blue', className, href, ...props }) {
 
     className = clsx(
         baseStyles[variant],
+        variantStyles[variant][color],
         className
     )
     return href
