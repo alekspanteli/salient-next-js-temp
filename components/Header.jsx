07764bb107a9c0656b3e222ad1d177d95427d1fd
alekspanteli <alekspanteli@rambler.ru>
2022-11-19 19:46:35 +0000

    fix btn

--- components/Header.jsx
+++ components/Header.jsx
@@ -38,7 +38,7 @@ export default function Header() {
                             <div className="hidden md:block">
                                 <NavLink href="/login">Sign in</NavLink>
                             </div>
-                            <Button href="/register" color="blue">
+                            <Button href="/register">
                                 <span>
                                     Get started <span className="hidden lg:inline">today</span>
                                 </span>
