f0a87d4febea999420a3566a407ab20ab19bb568
alekspanteli <alekspanteli@rambler.ru>
2022-11-22 16:57:41 +0000

    test

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,67 @@ import NavLink from '@/components/NavLink'
 import Container from '@/components/Container'
 import Button from '@/components/Btn'
 
+import { Popover, Transition } from '@headlessui/react'
+import clsx from 'clsx'
+
+function MobileNavLink({ href, children }) {
+    return (
+      <Popover.Button as={Link} href={href} className="block w-full p-2">
+        {children}
+      </Popover.Button>
+    )
+  }
+
+function MobileNavIcon({ open }) {
+    return (
+        <svg
+            aria-hidden="true"
+            className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
+            fill="none"
+            strokeWidth={2}
+            strokeLinecap="round"
+        >
+            <path
+                d="M0 1H14M0 7H14M0 13H14"
+                className={clsx(
+                    'origin-center transition',
+                    open && 'scale-90 opacity-0'
+                )}
+            />
+            <path
+                d="M2 2L12 12M12 2L2 12"
+                className={clsx(
+                    'origin-center transition',
+                    !open && 'scale-90 opacity-0'
+                )}
+            />
+        </svg>
+    )
+}
+
+function MobileNavigation() {
+    return (
+        <Popover>
+            <Popover.Button
+                className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
+                aria-label="Toggle Navigation"
+            >
+                {({ open }) => <MobileNavIcon open={open} />}
+            </Popover.Button>
+            <Popover.Panel
+            as="div"
+            className="absolute inset-x-0 top-full mt-4 flex origin-top flex-col rounded-2xl bg-white p-4 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5"
+          >
+            <MobileNavLink href="#features">Features</MobileNavLink>
+            <MobileNavLink href="#testimonials">Testimonials</MobileNavLink>
+            <MobileNavLink href="#pricing">Pricing</MobileNavLink>
+            <hr className="m-2 border-slate-300/40" />
+            <MobileNavLink href="/login">Sign in</MobileNavLink>
+          </Popover.Panel>
+        </Popover>
+    )
+}
+
 export default function Header() {
     return (
         <>
@@ -43,6 +104,9 @@ export default function Header() {
                                     Get started <span className="hidden lg:inline">today</span>
                                 </span>
                             </Button>
+                            <div className="-mr-1 md:hidden">
+                                <MobileNavigation />
+                            </div>
                         </div>
                     </nav>
                 </Container>
